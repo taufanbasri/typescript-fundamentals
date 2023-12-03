daba5f490b606c53b5b33caa09cdfcdb275fdc33
Taufan Prasetyo <taufanremote@gmail.com>
2023-12-03 22:34:33 +0700

    functions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-const enum Size {
-  Small = 1,
-  Medium,
-  Large,
+function calculateTax(income: number, taxYear = 2022): number {
+  if (taxYear < 2022) return income * 1.2;
+
+  return income * 1.3;
 }
 
-let mySize: Size = Size.Medium;
-
-console.log(mySize);
+calculateTax(10_000);
