a0236d27aefe9a5abb69789739de66ecba43ba96
Taufan Prasetyo <taufanremote@gmail.com>
2023-12-09 02:31:03 +0700

    Inheritance

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+class Person {
+  constructor(public firstName: string, public lastName: string) {}
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  walk() {
+    console.log("Walking");
+  }
+}
+
+class Student extends Person {
+  constructor(public studentId: number, firstName: string, lastName: string) {
+    super(firstName, lastName);
+  }
+
+  takeTest() {
+    console.log("Taking a test");
+  }
+}
+
+let student = new Student(1, "Taufan", "taufan@mail.com");
+student.walk();
