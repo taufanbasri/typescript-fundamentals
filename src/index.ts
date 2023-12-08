b167c1551a46c12db46905bd17fc3dd1a7eaf2f6
Taufan Prasetyo <taufanremote@gmail.com>
2023-12-09 01:55:25 +0700

    cleared

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-class SeatAssignment {
-  // Index signature property
-  [seatNumber: string]: string;
-}
-
-let seats = new SeatAssignment();
-seats.A1 = "Taufan";
-seats["A2"] = "Ahmad";
-console.log(seats);
