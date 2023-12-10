cbb5416123b00962cc747f5ac569b9dd73a13f4e
Taufan Prasetyo <taufanremote@gmail.com>
2023-12-10 07:14:30 +0700

    cleared

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-class KeyValuePair<K, V> {
-  constructor(public key: K, public value: V) {}
-}
-
-let pair = new KeyValuePair<number, string>(1, "Apple");
-let pair2 = new KeyValuePair('a', "Apple");
