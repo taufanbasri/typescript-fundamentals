adf0e59c525c9940f2b0bf9def6e812ba6277d7c
Taufan Prasetyo <taufanremote@gmail.com>
2023-12-10 13:30:50 +0700

    Generic Interfaces

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+interface Result<T> {
+  data: T | null;
+  error: string | null;
+}
+
+function fetch<T>(url: string): Result<T> {
+  return { data: null, error: null };
+}
+
+interface User {
+  username: string;
+}
+
+interface Product {
+  title: string;
+}
+
+let userResult = fetch<User>("url");
+let productResult = fetch<Product>("url");
