cbb4b8886734108b136d269fa7ecce4723fd4c52
Taufan Prasetyo <taufanremote@gmail.com>
2023-12-10 21:39:53 +0700

    Generic constrains

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+class Person {
+  constructor(public name: string) {}
+}
+
+class Customer extends Person {}
+
+function echo<T extends Person>(value: T): T {
+  return value;
+}
+
+echo(new Customer("a"));
