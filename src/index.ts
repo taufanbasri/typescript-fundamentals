80c0aa736ae31bd0c2e8f30cad1195577e3eee66
Taufan Prasetyo <taufanremote@gmail.com>
2023-12-11 19:46:51 +0700

    Decorator parameters

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
-function Component(constructor: Function) {
-  console.log("Component decorator called");
-  constructor.prototype.uniqueId = Date.now();
-  constructor.prototype.insertInDOM = () => {
-    console.log("Inserting the component in the DOM");
+type ComponentOptions = {
+  selector: string;
+};
+
+// Decorator factory
+function Component(options: ComponentOptions) {
+  return (constructor: Function) => {
+    console.log("Component decorator called");
+    constructor.prototype.options = options;
+    constructor.prototype.uniqueId = Date.now();
+    constructor.prototype.insertInDOM = () => {
+      console.log("Inserting the component in the DOM");
+    };
   };
 }
 
-@Component
+@Component({ selector: "#my-profile" })
 class ProfileComponent {}
