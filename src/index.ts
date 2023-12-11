64b1cfdc9ef5456aba6ef2f4b4f79b63886a0bc0
Taufan Prasetyo <taufanremote@gmail.com>
2023-12-11 19:43:25 +0700

    Class Decorators

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+function Component(constructor: Function) {
+  console.log("Component decorator called");
+  constructor.prototype.uniqueId = Date.now();
+  constructor.prototype.insertInDOM = () => {
+    console.log("Inserting the component in the DOM");
+  };
+}
+
+@Component
+class ProfileComponent {}
