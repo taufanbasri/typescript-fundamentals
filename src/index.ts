cbd3d15f4ef6c79ea73c0f636f3cafbb771e59be
Taufan Prasetyo <taufanremote@gmail.com>
2023-12-11 08:59:05 +0700

    Type Mapping

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,20 @@ interface Product {
   name: string;
   price: number;
 }
+
+type ReadOnly<T> = {
+  readonly [K in keyof T]: T[K];
+};
+
+type Optional<T> = {
+  [K in keyof T]?: T[K];
+};
+
+type Nullable<T> = {
+  [K in keyof T]: T[K] | null;
+};
+
+let product: ReadOnly<Product> = {
+  name: "a",
+  price: 1,
+};
