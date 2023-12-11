082e2dd3e1286f292477f2dc7cfae9404a1fb894
Taufan Prasetyo <taufanremote@gmail.com>
2023-12-11 08:15:53 +0700

    cleared

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,3 @@ interface Product {
   name: string;
   price: number;
 }
-
-class Store<T> {
-  protected _objects: T[] = [];
-
-  add(obj: T): void {
-    this._objects.push(obj);
-  }
-
-  find(property: keyof T, value: any): T | undefined {
-    return this._objects.find((obj) => obj[property] === value);
-  }
-}
-
-let store = new Store<Product>();
-store.add({ name: "T", price: 1 });
-store.find("name", "T");
-store.find("price", 1);
-store.find("nonExistingProperty", 1); // compiler error
